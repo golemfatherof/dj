40dcc635ff232e4e47c14e410bc70a5cabf12a64
golemfatherof <sminepl.ayzz@gmail.com>
2024-12-25 16:23:39 +0530

    Add files via upload

---
+++ rajaji.py
@@ -0,0 +1,16 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY TU ADMIN NHI HAI TU CHUTIYA HAI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("8018797794:AAEdFriqrYmOrKotJrJhxtTaZhka9WHAAhk")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=(" @GOLEM_OWNER")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("7584228621")
+
+
+
+
+  
