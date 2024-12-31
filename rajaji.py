a8bc703f18f696945591ba0202839cb6ab6b32d0
golemfatherof <sminepl.ayzz@gmail.com>
2024-12-31 17:56:49 +0530

    Update rajaji.py

--- rajaji.py
+++ rajaji.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7785979676:AAFo6mw4AVFdy56Bgz3onxu0e360A3_LACo")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=(" @GOLEM_OWNER")
+OWNER_USERNAME=("@BGMIDDOS1245")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1724246173")
